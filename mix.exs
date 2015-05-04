5bc3699ac1726d2142a6dcf6f23de62254a3a509
Matt Sawka <matt.sawka@perceptivesoftware.com>
2015-05-04 12:50:38 -0500

    bump orchestrator_api and messaging

--- mix.exs
+++ mix.exs
@@ -39,11 +39,11 @@ defmodule OpenAperture.Builder.Mixfile do
       {:ex_doc, github: "elixir-lang/ex_doc", only: [:test]},
       {:markdown, github: "devinus/markdown", only: [:test]},
             
-      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "dc50470d0f0026718913535c901aee1562868dbf", override: true},
+      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "b313cf059816389288d946ae022b702e22a7fe68", override: true},
       {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "ae629a4127acceac8a9791c85e5a0d3b67d1ad16", override: true},
       {:openaperture_overseer_api, git: "https://github.com/OpenAperture/overseer_api.git", ref: "4d65d2295f2730bc74ec695c32fa0d2478158182", override: true},
       {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "7aeca4655225fa0dd63f1465c0af30b0992b94b5", override: true},
-      {:openaperture_workflow_orchestrator_api, git: "https://github.com/OpenAperture/workflow_orchestrator_api.git", ref: "d91664c4c6b21f889cba40a623c8893f043f59c8", override: true},
+      {:openaperture_workflow_orchestrator_api, git: "https://github.com/OpenAperture/workflow_orchestrator_api.git", ref: "9f71efeda9ddf5315d3f7c945c5336347c720ac9", override: true},
       {:timex, "~> 0.12.9"},
       
       {:meck, "0.8.2", only: :test},
