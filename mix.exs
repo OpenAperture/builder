a01f7ba250a1fe1b4dad0afdd18f86971472bc71
Matt Sawka <matt.sawka@perceptivesoftware.com>
2015-05-04 13:55:51 -0500

    fix dependency issues

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule OpenAperture.Builder.Mixfile do
       {:markdown, github: "devinus/markdown", only: [:test]},
             
       {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "0af47ad04667342251bd829e01379b6d7703433f", override: true},
-      {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "ae629a4127acceac8a9791c85e5a0d3b67d1ad16", override: true},
+      {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "5d442cfbdd45e71c1101334e185d02baec3ef945", override: true},
       {:openaperture_overseer_api, git: "https://github.com/OpenAperture/overseer_api.git", ref: "4d65d2295f2730bc74ec695c32fa0d2478158182", override: true},
       {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "7aeca4655225fa0dd63f1465c0af30b0992b94b5", override: true},
       {:openaperture_workflow_orchestrator_api, git: "https://github.com/OpenAperture/workflow_orchestrator_api.git", ref: "9f71efeda9ddf5315d3f7c945c5336347c720ac9", override: true},
