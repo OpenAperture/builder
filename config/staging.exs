db9775f428361046c4c412c8cea7e0ab1b269064
Matt Sawka <matt.sawka@perceptivesoftware.com>
2015-05-12 08:41:03 -0500

    add staging env

---
+++ config/staging.exs
@@ -0,0 +1,21 @@
+use Mix.Config
+
+config :logger, level: :debug
+
+
+config :autostart,
+	register_queues: true
+
+config :openaperture_manager_api, 
+	manager_url: System.get_env("MANAGER_URL"),
+	oauth_login_url: System.get_env("OAUTH_LOGIN_URL"),
+	oauth_client_id: System.get_env("OAUTH_CLIENT_ID"),
+	oauth_client_secret: System.get_env("OAUTH_CLIENT_SECRET")
+
+config :github, 
+	user_credentials: System.get_env("GITHUB_OAUTH_TOKEN")
+
+config :openaperture_overseer_api,
+	module_type: :builder,
+	exchange_id: System.get_env("EXCHANGE_ID"),
+	broker_id: System.get_env("BROKER_ID")
